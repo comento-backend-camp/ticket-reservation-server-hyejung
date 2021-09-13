fc57beb0bfe5e8bea420be6bcc400afe57767bee
hyejung <kimhyejung12@naver.com>
2021-09-13 19:48:56 +0900

    [feat:#7] 프로젝트 초기 설정

---
+++ ticket/src/test/java/comento/backend/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package comento.backend.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
